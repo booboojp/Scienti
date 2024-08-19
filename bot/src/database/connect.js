fa48f10bbd44085bd4945fa5ebf230839731b2d9
booboo <>
2024-08-18 23:42:45 -0400

    Errors not fixed, commit before share.

--- bot/src/database/connect.js
+++ bot/src/database/connect.js
@@ -3,6 +3,24 @@ const ansis = require('ansis');
 require('dotenv').config();	
 
 async function connect() {
+    mongoose.set('strictQuery', false);
+    try {
+        console.log('Trying to connect to DB');
+        await mongoose.connect(process.env.MONGO_TOKEN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch {
+        console.log('Failed to connect to MongoDB.')
+        process.exit(1)
+    }
 
+    mongoose.connection.once("open", () => {
+        console.log("MongoDB is connected!");
+    })
+    mongoose.connection.on("error", () => {
+        console.log("Error, exiting.");
+        process.exit(1);
+    })
 }
 module.exports = connect
