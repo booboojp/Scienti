9a35c3223de36dd5da46efdc6f674d9620654191
booboo <>
2024-08-18 23:28:03 -0400

    Created database folder and started on subCommands function, along with function file.

---
+++ bot/src/database/connect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const ansis = require('ansis');
+require('dotenv').config();	
+
+async function connect() {
+
+}
+module.exports = connect
---
+++ bot/src/handlers/functions/functions.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const fetch = reqire('node-fetch');
+
+module.exports = async (client) => {
+    client.loadSubcommands = async function (client, interaction, args) {
+        try {
+
+        } catch {
+            return
+        }
+    }
+}
